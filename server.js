f5bb779bbed432053c2931a217a9705eb7d842b2
carlosvalarezo <carlos.valarezo@gmail.com>
2020-07-23 11:17:17 -0500

    [@carlosvalarezo] moves queue message from backend to bff

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const express = require('express');
 const http = require('http');
 const app = express();
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const csv = require('csvtojson');
 
 const PORT = process.env.PORT || 8002;
 
@@ -13,8 +16,22 @@ const io = require("socket.io");
 const server = io.listen(httpServer);
 
 const apiClient = require('./clients/ApiChatClient');
-const rabbitConsumer = require('./lib/rabbit/RabbitConsumer');
-const mongoConsumer = require('./lib/rabbit/MongoConsumer');
+const QueueSender = require('./lib/rabbit/QueueSender');
+const RabbitConsumer = require('./lib/rabbit/RabbitConsumer');
+
+const validateMessage = async (message) => {
+  if (message.search("/stock=") >= 0){
+    const code = message.split("=")[1];
+    const stooqResponse = await axios.get(`https://stooq.com/q/l/?s=${code}&f=sd2t2ohlcv&h&e=csv`);
+    const {data} = stooqResponse;
+    return csv({output: "json"}).fromString(data).then( row=>{
+      const message = `${row[0].Symbol} quote is \$${row[0].Close}`;
+      const newMessage = {name: "bot", status:message};
+      QueueSender.send('chat-queue', newMessage);
+      return new Promise(resolve => resolve(newMessage));
+    });
+  }
+}
 
 server.on("connection", socket => {
     socket.emit('start', 'server starts...');
@@ -24,18 +41,24 @@ server.on("connection", socket => {
         console.info(`Client gone `);
     });
 
-    socket.on("getMessages", async jwt => {
+    socket.on("getMessages", async token => {
       try{
-        const msgs = await apiClient.getMessages(jwt);
+        const msgs = await apiClient.getMessages(token);
       }
       catch(err){
         console.log(err);
       }
     })
 
-    socket.on("new-message", async (jwt, message) => {
+    socket.on("new-message", async (token, message) => {
         try{
-          await apiClient.insertMessage(jwt, message);
+            const decoded = jwt.verify(token, "mysecret");
+            // console.log(message);
+            // console.log(decoded);
+          //const user = await apiClient.insertMessage(jwt, message);
+            QueueSender.send('chat-queue',{name:decoded.user, status:message});
+            await validateMessage(message);
+
         }
         catch(err){
           console.log(err);
